a0bb86d3477ef07fda058dd2882416bdfeec953e
Jewel-Thomas <jewelthomas20@gmail.com>
2025-08-04 15:39:55 +0530

    handled custom collision using raycast(capsuleCast).

--- Cooking Magic/Assets/Scripts/Player.cs
+++ Cooking Magic/Assets/Scripts/Player.cs
@@ -11,8 +11,30 @@ public class Player : MonoBehaviour
     {
         Vector2 inputVector = gameInput.PlayerMovementInputNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        float moveDistance = Time.deltaTime * moveSpeed;
+        float playerRadius = 0.7f;
+        float playerHeight = 2f;
+
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        if (!canMove)
+        {
+            Vector3 moveDirX = new Vector3(inputVector.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+            if (canMove) moveDir = moveDirX;
+            else
+            {
+                Vector3 moveDirZ = new Vector3(0, 0, inputVector.y).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+                if (canMove) moveDir = moveDirZ;
+            }
+        }
+
+        if (canMove) transform.position += moveDir * moveDistance;
         isWalking = moveDir != Vector3.zero;
-        transform.position += moveDir * Time.deltaTime * moveSpeed;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
     }
 
