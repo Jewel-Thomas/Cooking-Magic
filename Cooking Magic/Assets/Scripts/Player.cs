4a060d16dbb7e9cd6d65e96772411aaa86931012
Jewel-Thomas <jewelthomas20@gmail.com>
2025-08-04 12:49:30 +0530

    Animated player with simple animations.

--- Cooking Magic/Assets/Scripts/Player.cs
+++ Cooking Magic/Assets/Scripts/Player.cs
@@ -1,11 +1,14 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float rotationSpeed = 10f;
+    private bool isWalking = false;
     private void Update()
     {
-        
+
         Vector2 inputVector = new Vector2(0, 0);
 
         if (Input.GetKey(KeyCode.W))
@@ -28,8 +31,13 @@ public class Player : MonoBehaviour
         inputVector = inputVector.normalized;
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        isWalking = moveDir != Vector3.zero;
         transform.position += moveDir * Time.deltaTime * moveSpeed;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
+    }
 
-
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
