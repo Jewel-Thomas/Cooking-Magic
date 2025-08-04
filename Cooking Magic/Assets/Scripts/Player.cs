c8bb9d295c8f58ba094208a2962d69550192034a
Jewel-Thomas <jewelthomas20@gmail.com>
2025-08-04 15:03:28 +0530

    refactored input with the new input system and added multiple bindings for movements including support for joystick.

--- Cooking Magic/Assets/Scripts/Player.cs
+++ Cooking Magic/Assets/Scripts/Player.cs
@@ -5,31 +5,11 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float rotationSpeed = 10f;
+    [SerializeField] private GameInput gameInput;
     private bool isWalking = false;
     private void Update()
     {
-
-        Vector2 inputVector = new Vector2(0, 0);
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = 1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = 1;
-        }
-
-        inputVector = inputVector.normalized;
-
+        Vector2 inputVector = gameInput.PlayerMovementInputNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         isWalking = moveDir != Vector3.zero;
         transform.position += moveDir * Time.deltaTime * moveSpeed;
