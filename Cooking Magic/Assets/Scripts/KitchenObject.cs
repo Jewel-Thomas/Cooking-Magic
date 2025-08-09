6facf6a7c144e7ff93319296ce7b2ec706780902
Jewel-Thomas <jewelthomas20@gmail.com>
2025-08-09 22:50:46 +0530

    handled kitchen object and counter parenting for correct object references.

--- Cooking Magic/Assets/Scripts/KitchenObject.cs
+++ Cooking Magic/Assets/Scripts/KitchenObject.cs
@@ -3,9 +3,34 @@ using UnityEngine;
 public class KitchenObject : MonoBehaviour
 {
     [SerializeField] private KitchenObjectsSO kitchenObjectsSO;
+    private ClearCounter clearCounter;
 
     public KitchenObjectsSO GetKitchenObjectsSO()
     {
         return kitchenObjectsSO;
+    }
+
+    public void SetClearCounter(ClearCounter clearCounter)
+    {
+        if (this.clearCounter != null)
+        {
+            this.clearCounter.ClearKitchenObject();
+        }
+
+        this.clearCounter = clearCounter;
+
+        if (clearCounter.HasKitchenObject())
+        {
+            Debug.LogError("Already has a kitchen object");
+        }
+        clearCounter.SetKitchenObject(this);
+
+        transform.parent = clearCounter.GetCounterTopPoint();
+        transform.localPosition = Vector3.zero;
+    }
+
+    public ClearCounter GetClearCounter()
+    {
+        return clearCounter;
     }
 }
