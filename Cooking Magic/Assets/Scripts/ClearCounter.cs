7c395a43fe7173d01614e71794294a2daaa56137
Jewel-Thomas <jewelthomas20@gmail.com>
2025-08-09 21:34:41 +0530

    made use of scriptale object to instantiate an ingredient.

--- Cooking Magic/Assets/Scripts/ClearCounter.cs
+++ Cooking Magic/Assets/Scripts/ClearCounter.cs
@@ -3,8 +3,12 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private KitchenObjectsSO kitchenObject;
+    [SerializeField] private Transform counterTopPoint;
     public void Interact()
     {
         Debug.Log("interact!");
+        Transform tomatoTransform = Instantiate(kitchenObject.prefab, counterTopPoint);
+        tomatoTransform.localPosition = Vector3.zero;
     } 
 }
