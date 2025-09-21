c4c063ffc65d4820c066bf558f2985d5fb299906
Jewel-Thomas <80634999+Jewel-Thomas@users.noreply.github.com>
2025-09-21 20:55:25 +0530

    have added a main menu scene, loading scene with new game logo

---
+++ Cooking Magic/Assets/Scripts/Loader.cs
@@ -0,0 +1,25 @@
+using UnityEngine.SceneManagement;
+
+public static class Loader
+{
+    public enum Scene
+    {
+        MainMenu,
+        LoadingScene,
+        GameScene
+    }
+
+    public static Scene targetScene;
+
+    public static void Load(Scene targetScene)
+    {
+        Loader.targetScene = targetScene;
+
+        SceneManager.LoadScene(Scene.LoadingScene.ToString());
+    }
+
+    public static void LoaderCallback()
+    {
+        SceneManager.LoadScene(targetScene.ToString());
+    }
+}
