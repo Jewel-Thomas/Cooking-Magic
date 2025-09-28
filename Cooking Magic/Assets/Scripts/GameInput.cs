299a99c02c3b79e508f93ef81074a98936a52d8c
Jewel-Thomas <80634999+Jewel-Thomas@users.noreply.github.com>
2025-09-28 14:59:27 +0530

    saved key binding configuration using player prefs

--- Cooking Magic/Assets/Scripts/GameInput.cs
+++ Cooking Magic/Assets/Scripts/GameInput.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 
 public class GameInput : MonoBehaviour
 {
+    private const string PLAYER_PREFS_KEY_BINDINGS = "KeyBindings";
+
     public static GameInput Instance { get; private set; }
 
     public event EventHandler OnInteractAction;
@@ -26,6 +28,12 @@ public class GameInput : MonoBehaviour
     {
         Instance = this;
         playerInputActions = new PlayerInputActions();
+
+        if(PlayerPrefs.HasKey(PLAYER_PREFS_KEY_BINDINGS))
+        {
+            playerInputActions.LoadBindingOverridesFromJson(PlayerPrefs.GetString(PLAYER_PREFS_KEY_BINDINGS));
+        }
+
         playerInputActions.Player.Enable();
         playerInputActions.Player.Interact.performed += Interact_performed;
         playerInputActions.Player.InteractAlternate.performed += InteractAlternate_performed;
@@ -130,8 +138,11 @@ public class GameInput : MonoBehaviour
             .OnComplete(callback =>
             {
                 callback.Dispose();
-                OnActionRebound();
                 playerInputActions.Enable();
+                OnActionRebound();
+
+                PlayerPrefs.SetString(PLAYER_PREFS_KEY_BINDINGS, playerInputActions.SaveBindingOverridesAsJson());
+                PlayerPrefs.Save();
             })
             .Start();
     }
